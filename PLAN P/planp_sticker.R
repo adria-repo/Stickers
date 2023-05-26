8511af7b1c1b249cf9d59386955f84a1a0a92b38
adria-repo <134696382+adria-repo@users.noreply.github.com>
2023-05-26 17:34:30 +0200

    Update planp_sticker.R

--- PLAN P/planp_sticker.R
+++ PLAN P/planp_sticker.R
@@ -6,4 +6,4 @@ sticker("Logo Plan P.png", package=" ",
         s_width=0.7,
         h_fill="#FFFFFF", 
         h_color="#72962c", 
-        filename="inst/figures/planp.png")
+        filename="planp.png")
