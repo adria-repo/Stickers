2a18f8fc6893f5f8bccfa25adfe28bc04e4e286d
adria-repo <134696382+adria-repo@users.noreply.github.com>
2023-05-26 17:33:13 +0200

    Add files via upload

---
+++ PLAN P/planp_sticker.R
@@ -0,0 +1,10 @@
+library(hexSticker)
+
+sticker("Logo Plan P.png", package=" ", 
+        #p_size=20, 
+        s_x=1, 
+        s_y=1, 
+        s_width=0.7,
+        h_fill="#FFFFFF", 
+        h_color="#72962c", 
+        filename="inst/figures/planp.png")
